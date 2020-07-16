d6b09d4b40103238b28e1f5d9036345ea98092e8
Wonbae Kim <wbkim@unist.ac.kr>
2020-07-16 22:47:34 +0900

    test python worker

---
+++ test/ptre_worker.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import ptre_test_common
+
+import ptre.tensorflow.keras as ptre
+
+ptre.init()
+
+while True:
+  pass
