4026d0ab9e369d011db16f6c8f9b65d0f11192a1
Wonbae Kim <wbkim@unist.ac.kr>
2020-09-09 14:52:17 +0900

    DBG and DBGR

--- ptre/common/logging.h
+++ ptre/common/logging.h
@@ -3,4 +3,9 @@
 
 #include "tensorflow/core/platform/logging.h"
 
+#define DBG(x) if (x == "predictions_kernel_0") DVLOG(0)
+#define DBGR(x, r) \
+    if (x == "predictions_kernel_0") \
+      DVLOGR(0, r) << __FUNCTION__ << "] "
+
 #endif  // PTRE_COMMON_LOGGING_H_
