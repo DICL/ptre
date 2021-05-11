6fb044d53b3ae128f14c0b6561f73501122acd23
Wonbae Kim <wbkim@unist.ac.kr>
2021-05-11 17:37:42 +0900

    use os env

--- ptre/python/ptre/tensorflow/ptre_ops.py
+++ ptre/python/ptre/tensorflow/ptre_ops.py
@@ -2,14 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import re
 import ctypes
+import os
+import re
 import time
 
 from tensorflow.python.framework import load_library
 from ptre.tensorflow.util import _executing_eagerly
 
-PTRE_LIB_PATH = '/home/wkim/ptre/build/ptre/tensorflow/kernels/libptre_ops.so'
+PTRE_LIB_PATH = os.environ['PTRE_SHARED_LIB']
+#PTRE_LIB_PATH = '/home/wkim/ptre/build/ptre/tensorflow/kernels/libptre_ops.so'
 #PTRE_LIB_PATH = '/home/wkim/.local/lib/python2.7/site-packages/ptre/tensorflow/libptre_ops.so'
 PTRE_LIB = load_library.load_op_library(PTRE_LIB_PATH)
 PTRE_CDLL = ctypes.CDLL(PTRE_LIB_PATH, mode=ctypes.RTLD_GLOBAL)
